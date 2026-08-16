c65b51703bcd1b2e4a4e55ffbba6b83f93ccfb51
niftyalbert-art <niftyalbert@gmail.com>
2026-08-16 20:22:59 +0000

    security: display auto-generated nanivio user pin badges inside account profiles

--- artifacts/nifty-pay/dev-dist/sw.js
+++ artifacts/nifty-pay/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.trdojmif2lc"
+    "revision": "0.88luvtcajjk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
